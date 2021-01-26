ca1c79424ff00f5d364eecd2f4f58bf55933cd29
riqueenz <henriqueenzweiler@gmail.com>
2021-01-26 22:16:10 +0100

    local_data_files module added

---
+++ src/settings/local_data_files.py
@@ -0,0 +1,12 @@
+def create():
+    settings_file = "cutting start command" + "\n"
+    settings_file += "M07" + "\n" + "\n"
+    settings_file = "cutting end command" + "\n"
+    settings_file += "M08" + "\n" + "\n"
+    settings_file = "gcode file extension" + "\n"
+    settings_file += ".CNC" + "\n" + "\n"
+    settings_file = "table size X (mm)" + "\n"
+    settings_file += "2700" + "\n" + "\n"
+    settings_file = "table size Y (mm)" + "\n"
+    settings_file += "6300" + "\n" + "\n"
+    print(settings_file);
