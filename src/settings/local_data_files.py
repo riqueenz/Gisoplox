ac679eb088f993bffd95fd87f438d808022d2095
riqueenz <henriqueenzweiler@gmail.com>
2021-01-27 10:49:24 +0100

    Added lines to create a .ini file

--- src/settings/local_data_files.py
+++ src/settings/local_data_files.py
@@ -1,12 +1,27 @@
 def create():
-    settings_file = "cutting start command" + "\n"
-    settings_file += "M07" + "\n" + "\n"
-    settings_file = "cutting end command" + "\n"
-    settings_file += "M08" + "\n" + "\n"
-    settings_file = "gcode file extension" + "\n"
-    settings_file += ".CNC" + "\n" + "\n"
-    settings_file = "table size X (mm)" + "\n"
-    settings_file += "2700" + "\n" + "\n"
-    settings_file = "table size Y (mm)" + "\n"
-    settings_file += "6300" + "\n" + "\n"
-    print(settings_file);
+    settings_file_text = "cutting start command" + "\n"
+    settings_file_text += "M07" + "\n" + "\n"
+    settings_file_text += "cutting end command" + "\n"
+    settings_file_text += "M08" + "\n" + "\n"
+    settings_file_text += "gcode file extension" + "\n"
+    settings_file_text += ".CNC" + "\n" + "\n"
+    settings_file_text += "table size X (mm)" + "\n"
+    settings_file_text += "2700" + "\n" + "\n"
+    settings_file_text += "table size Y (mm)" + "\n"
+    settings_file_text += "6300" + "\n" + "\n"
+    settings_file_text += "show line numbers (0-no, 1-yes)" + "\n"
+    settings_file_text += "0" + "\n" + "\n"
+    settings_file_text += "show feed speed (0-no, 1-yes)" + "\n"
+    settings_file_text += "0" + "\n" + "\n"
+    settings_file_text += "show positioning speed (0-no, 1-yes)" + "\n"
+    settings_file_text += "0" + "\n" + "\n"
+    settings_file_text += "positioning speed" + "\n"
+    settings_file_text += "2700" + "\n" + "\n"
+    settings_file_text += "default save folder Linux" + "\n"
+    settings_file_text += "/media/a/OXICORTE" + "\n" + "\n"
+    settings_file_text += "default save folder Windows" + "\n"
+    settings_file_text += "" + "\n" + "\n"
+    #print(settings_file_text);
+    settings_file = open("settings.ini", "w")
+    settings_file.write(settings_file_text)
+    settings_file.close()
