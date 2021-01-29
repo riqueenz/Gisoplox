7e26283dbf798345504e5218e4ad9e76061e114a
riqueenz <henriqueenzweiler@gmail.com>
2021-01-29 11:40:33 +0100

    Modified module import

--- src/settings/local_data_files.py
+++ src/settings/local_data_files.py
@@ -1,6 +1,6 @@
 import os
-import settings_file
-import oxyfuel_cutting_width_file
+from settings import oxyfuel_cutting_width_file
+from settings import settings_file
 def create():
     os.mkdir(".gisoplox")
     settings_file.create()
