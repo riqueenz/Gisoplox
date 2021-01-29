161fc4f810ccca1145ec50bf31e1434b3820e69d
riqueenz <henriqueenzweiler@gmail.com>
2021-01-29 12:05:33 +0100

    Add plasma cutting settings file creation

--- src/settings/local_data_files.py
+++ src/settings/local_data_files.py
@@ -1,7 +1,9 @@
 import os
 from settings import oxyfuel_cutting_width_file
+from settings import plasma_cutting_width_file
 from settings import settings_file
 def create():
     os.mkdir(".gisoplox")
     settings_file.create()
     oxyfuel_cutting_width_file.create()
+    plasma_cutting_width_file.create()
