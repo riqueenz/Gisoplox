1ff23bb9c1323380526cdf9e70606df2aa2bb4b0
riqueenz <henriqueenzweiler@gmail.com>
2021-01-30 10:46:04 +0100

    added history file

---
+++ src/settings/history_file.py
@@ -0,0 +1,4 @@
+def create():
+    history_file = open(".gisoplox/history.gisoplox", "w")
+    history_file.write("")
+    history_file.close()
--- src/settings/local_data_files.py
+++ src/settings/local_data_files.py
@@ -3,9 +3,12 @@ from settings import oxyfuel_cutting_width_file
 from settings import plasma_cutting_width_file
 from settings import settings_file
 from settings import metrics_file
+from settings import history_file
+
 def create():
     os.mkdir(".gisoplox")
     settings_file.create()
     oxyfuel_cutting_width_file.create()
     plasma_cutting_width_file.create()
     metrics_file.create()
+    history_file.create()
