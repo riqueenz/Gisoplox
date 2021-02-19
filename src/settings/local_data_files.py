2fa258d1b39c22b7d23e12aeb4fd714cac1b6db5
riqueenz <henriqueenzweiler@gmail.com>
2021-02-19 13:05:02 +0100

    copy files from previous snap revisions

--- src/settings/local_data_files.py
+++ src/settings/local_data_files.py
@@ -6,11 +6,15 @@ from settings import metrics_file
 from settings import history_file
 
 
-def create():
+def create_data_folders():
     if not os.path.exists(".gisoplox"):
         os.mkdir(".gisoplox")
     if not os.path.exists("Gisoplox_output"):
         os.mkdir("Gisoplox_output")
+
+
+def create():
+    create_data_folders()
     settings_file.create()
     oxyfuel_cutting_width_file.create()
     plasma_cutting_width_file.create()
--- src/settings/snap.py
+++ src/settings/snap.py
@@ -1,39 +1,69 @@
 import os
 import shutil
+from settings import local_data_files
 
 
-def change_python_cwd():
+def find_snap_folders():
+    current_snap_folder = ""
+    old_snap_folder = ""
     snap_cwd = os.getcwd() + "/snap/gisoplox/"
     if os.path.exists(snap_cwd):
         x = 10000
         while x >= 1:
             current_snap_folder = snap_cwd + str(x)
-            old_snap_folder = snap_cwd + str(x - 1)
-            # print(old_snap_folder)
-            print(current_snap_folder)
-            if os.path.exists(old_snap_folder) and os.path.exists(current_snap_folder):
-                os.mkdir(current_snap_folder + str("/.gisoplox"))
-                os.mkdir(current_snap_folder + str("/Gisoplox_output"))
-                old_file = old_snap_folder + "/.gisoplox/history.gisoplox"
-                new_file = current_snap_folder + "/.gisoplox/history.gisoplox"
-                shutil.copy(old_file, new_file)
-                old_file = old_snap_folder + "/.gisoplox/history_sheet.csv"
-                new_file = current_snap_folder + "/.gisoplox/history_sheet.csv"
-                shutil.copy(old_file, new_file)
-                old_file = old_snap_folder + "/.gisoplox/metrics.gisoplox"
-                new_file = current_snap_folder + "/.gisoplox/metrics.gisoplox"
-                shutil.copy(old_file, new_file)
-                old_file = old_snap_folder + "/.gisoplox/oxyfuel_cutting_width.gisoplox"
-                new_file = current_snap_folder + "/.gisoplox/oxyfuel_cutting_width.gisoplox"
-                shutil.copy(old_file, new_file)
-                old_file = old_snap_folder + "/.gisoplox/plasma_cutting_width.gisoplox"
-                new_file = current_snap_folder + "/.gisoplox/plasma_cutting_width.gisoplox"
-                shutil.copy(old_file, new_file)
-                old_file = old_snap_folder + "/.gisoplox/settings.ini"
-                new_file = current_snap_folder + "/.gisoplox/settings.ini"
-                shutil.copy(old_file, new_file)
-                x = 0
             if os.path.exists(current_snap_folder):
-                os.chdir(current_snap_folder)
-                x = 0
+                x -= 1
+                while x >= 1:
+                    old_snap_folder = snap_cwd + str(x)
+                    if os.path.exists(old_snap_folder):
+                        x = 0
+                    x -= 1
             x -= 1
+        return old_snap_folder, current_snap_folder
+
+
+def copy_files(old_snap_folder, current_snap_folder):
+    old_file = old_snap_folder + "/.gisoplox/history.gisoplox"
+    new_file = current_snap_folder + "/.gisoplox/history.gisoplox"
+    shutil.copy(old_file, new_file)
+    old_file = old_snap_folder + "/.gisoplox/history_sheet.csv"
+    new_file = current_snap_folder + "/.gisoplox/history_sheet.csv"
+    shutil.copy(old_file, new_file)
+    old_file = old_snap_folder + "/.gisoplox/metrics.gisoplox"
+    new_file = current_snap_folder + "/.gisoplox/metrics.gisoplox"
+    shutil.copy(old_file, new_file)
+    old_file = old_snap_folder + "/.gisoplox/oxyfuel_cutting_width.gisoplox"
+    new_file = current_snap_folder + "/.gisoplox/oxyfuel_cutting_width.gisoplox"
+    shutil.copy(old_file, new_file)
+    old_file = old_snap_folder + "/.gisoplox/plasma_cutting_width.gisoplox"
+    new_file = current_snap_folder + "/.gisoplox/plasma_cutting_width.gisoplox"
+    shutil.copy(old_file, new_file)
+    old_file = old_snap_folder + "/.gisoplox/settings.ini"
+    new_file = current_snap_folder + "/.gisoplox/settings.ini"
+    shutil.copy(old_file, new_file)
+
+
+def change_settings_file(old_snap_folder, current_snap_folder):
+    settings_file_name = current_snap_folder + '/.gisoplox/settings.ini'
+    settings_file = open(settings_file_name, 'r')
+    file_content = settings_file.read()
+    file_content = file_content.replace(old_snap_folder, current_snap_folder)
+    settings_file = open(settings_file_name, 'w')
+    settings_file.write(file_content)
+    settings_file.close()
+
+
+def change_python_cwd():
+    old_snap_folder, current_snap_folder = find_snap_folders()
+    if os.path.exists(current_snap_folder):
+        os.chdir(current_snap_folder)
+        local_data_files.create_data_folders()
+    settings_file = current_snap_folder + '/.gisoplox/settings.ini'
+    if not os.path.exists(settings_file):
+        if os.path.exists(old_snap_folder):
+            copy_files(old_snap_folder, current_snap_folder)
+            change_settings_file(old_snap_folder, current_snap_folder)
+        else:
+            local_data_files.create()
+
+
