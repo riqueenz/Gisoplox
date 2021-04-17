777398f40375f88b46243d35260e6acf7d1d37b4
riqueenz <henriqueenzweiler@gmail.com>
2021-04-18 00:26:42 +0200

    Delete src directory

--- src/settings/history_file.py
+++
@@ -1,7 +0,0 @@
-def create():
-    history_file = open(".gisoplox/history.gisoplox", "w")
-    history_file.write("")
-    history_file.close()
-    history_file = open(".gisoplox/history_sheet.csv", "w")
-    history_file.write("")
-    history_file.close()
